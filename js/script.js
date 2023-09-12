297eb0a9f72f691bd21f4d83bf21353017961667
SAHREZ <sahrezml@gmail.com>
2023-09-12 13:29:15 +0700

    menambah menu bar kanan

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika di klik
+
+document.querySelector("#semenmenu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
